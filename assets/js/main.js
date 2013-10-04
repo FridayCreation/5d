0f9bcae4085c0042b3e690235e4cf9514d238ac3
centwave <centwave@gmail.com>
2013-10-04 17:14:16 +0800

    add project planner

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,55 @@
-var types = ["Website Design & Development", "eCommerce", "Responsive Website", "Content Management System", "Print", "Mobile App", "Branding", "Searching Engine Optimisation"];
+var Categories = ["Website Design & Development", "eCommerce", "Responsive Website", "Content Management System", "Print", "Mobile App", "Branding", "Searching Engine Optimisation"];
 
-var ProjectPlannerModel = function(pTypes){
+var ProjectPlannerModel = function(Categories){
+
 	var self = this;
-	this.types = ko.observableArray(pTypes);
+
+	this.categories = ko.observableArray(Categories);
 	this.name = ko.observable("");
 	this.company = ko.observable("");
 	this.email = ko.observable("");
 	this.phone = ko.observable("");
 	this.message = ko.observable("");
-	this.selectedTypes = [];
+	this.selectedCategories = new Array();
+	this.errors = ko.observableArray([]);
 
+	this.onSelected = function(element, index){
+		
+		var $elem = $(element);
+
+		if ($elem.hasClass("active"))
+		{
+			var index = self.selectedCategories.indexOf(self.categories()[index]);
+			if (index > -1) self.selectedCategories.splice(index, 1);
+
+			$(element).removeClass("active");
+			$(element).children("i").removeClass("icon-ok").addClass("icon-remove");
+		}
+		else
+		{
+			self.selectedCategories.push(self.categories()[index]);
+
+			$(element).addClass("active");
+			$(element).children("i").removeClass("icon-remove").addClass("icon-ok");
+		}
+	}
+
+	this.onSubmited = function()
+	{
+		var errors = new Array();
+
+		if (self.name().length <= 0) errors.push("Require your name");
+		if (self.email().length <= 0) errors.push("Email address can't be empty");
+		if (self.phone().length <= 0) errors.push("Phone can't be empty");
+
+		self.errors(errors);
+
+		var result = ko.toJSON(self, function(key, value) { 
+			if (key == "categories") 
+				return undefined;
+			return value;
+    	});
+	}
 }
 
-ko.bindModelView(new ProjectPlannerModel());
+ko.applyBindings(new ProjectPlannerModel(Categories));
